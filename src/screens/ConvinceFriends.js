7edc801fb4f8f14fe4d47c534f70bc5d4c27f685
Anna Olchowik <ania.olchowik@gmail.com>
2019-10-15 19:47:21 +0200

    new convince friends component

--- src/screens/ConvinceFriends.js
+++ src/screens/ConvinceFriends.js
@@ -1,37 +1,22 @@
 import React, { Component } from 'react';
-//import { Button } from 'react-native';
+import { Button, Text } from 'react-native';
 import {StyleSheet} from 'react-native';
-import { Button, Container, Text ,Header, Left, Body, Title, Icon, Right} from 'native-base';
+
 
 class ConvinceFriends extends React.Component {
   static navigationOptions = {
-    title: 'Welcome to ProfileScreen',
+    title: 'Welcome to Convince Friends',
   };
   render() {
     const {navigate} = this.props.navigation;
     return (
-       <Container>
-      
+      <Text>
+      jkkjjkkhj
+      </Text>
         //onPress={() => navigate('Profile', {name: 'Jane'})}
-       </Container>
     );
   }
 }
-/*
-ConvinceFriends.navigationOptions = ({ navigation }) => ({
-  header: (
-    <Header>
-      <Left>
-        <Button transparent onPress={() => navigation.goBack(null)}>
-          <Icon name="return" />
-        </Button>
-      </Left>
-      <Body>
-        <Title>Profile</Title>
-      </Body>
-      <Right />
-    </Header>
-  )
-});*/
+
 
 export default ConvinceFriends;
