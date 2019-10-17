badea742a4a047d7c8c86e5cefbe90563b2ecce0
Basia Klosowska <basia.m.klosowska@gmail.com>
2019-10-17 23:47:21 +0200

    Add 'convince friends' page content #1

--- src/screens/ConvinceFriends.js
+++ src/screens/ConvinceFriends.js
@@ -1,22 +1,45 @@
-import React, { Component } from 'react';
-import { Button, Text } from 'react-native';
-import {StyleSheet} from 'react-native';
+import React from 'react';
+import { Text, Button, Alert } from 'react-native';
+import { StyleSheet } from 'react-native';
+import LinearGradient from 'components/shared/LinearGradient.js';
 
+import colors from 'constants/colors.js';
+const ConvinceFriends = () => (
+  <LinearGradient>
+    <Text style={styles.encouregment}>
+      No one can talk to your friends better than YOU.
+        </Text>
+    <Text style={styles.encouregment}>
+      You know what's important to them.
+        </Text>
+    <Text style={styles.encouregment}>
+      Let's convince them to support our common cause TOGETHER
+        </Text>
+    <Button
+      title="Let's do it"
+      onPress={() => Alert.alert('You did it!')} //onPress={() => navigate('Profile', {name: 'Jane'})}
+      color={colors.bg1}
+    />
+    <Text style={styles.securityNote}>We do not collect your data.</Text>
+    <Text style={styles.securityNote}>What you do here is between you and your friends.</Text>
+  </LinearGradient>
+);
 
-class ConvinceFriends extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome to Convince Friends',
-  };
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <Text>
-      jkkjjkkhj
-      </Text>
-        //onPress={() => navigate('Profile', {name: 'Jane'})}
-    );
+ConvinceFriends.navigationOptions = {
+  title: 'Welcome to Convince Friends',
+};
+
+const styles = StyleSheet.create({
+  encouregment: {
+    color: '#ffffff'
+  },
+  button: {
+    backgroundColor: colors.bg1
+  },
+  securityNote: {
+    textTransform: 'uppercase',
+    color: '#ffffff'
   }
-}
-
+});
 
 export default ConvinceFriends;
