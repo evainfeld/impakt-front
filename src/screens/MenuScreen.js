f63fb69cd6481cf369b28ddb0da212c12ff39b0c
Basia Klosowska <basia.m.klosowska@gmail.com>
2019-11-05 22:53:15 +0100

    Clean Menu screen

--- src/screens/MenuScreen.js
+++ src/screens/MenuScreen.js
@@ -1,63 +1,46 @@
-import React, { Component } from 'react';
-import { Button, Container,Content, Header,Text , Left, Body, Title, Icon, Right} from 'native-base';
+import React from 'react';
+import { Button, Text } from 'native-base';
+import { View, StyleSheet } from 'react-native';
 
-import { View , StyleSheet} from 'react-native';
+import MainLayout from 'components/layouts/MainLayout.js'
 
-class MenuScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome to Menu Screen',
-  };
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <Container >
-        <View style={styles.container}>
-          <Button block light large   style={styles.button} onPress={() => navigate('GetInvolved', {name: 'Jane'})}>  
-              <Text  >Get Involved</Text>
-          </Button>
-          <Button  block large style={styles.button} onPress={() => navigate('ConvinceFriendsInitial')}>
-             <Text  >Convince Friends</Text>
-          </Button>
-          <Button block large style={styles.button} onPress={() => navigate('NewsEvents', {name: 'Jane'})}>
-             <Text >News & Events</Text>
-          </Button>
-          <Button block large style={styles.button}  onPress={() => navigate('LocalChat', {name: 'Jane'})}>
-             <Text > Local Chat</Text>
-          </Button>
-        </View>
-      </Container>
-    );
-  }
+const MenuScreen = ({ navigation }) => {
+  const { navigate } = navigation;
+  return (
+    <MainLayout>
+      <View style={styles.container}>
+        <Button block light full large style={styles.button} onPress={() => navigate('GetInvolved', { name: 'Jane' })}>
+          <Text>Get Involved</Text>
+        </Button>
+        <Button block light full large style={styles.button} onPress={() => navigate('ConvinceFriendsInitial')}>
+          <Text>Convince Friends</Text>
+        </Button>
+        <Button block light full large style={styles.button} onPress={() => navigate('NewsEvents', { name: 'Jane' })}>
+          <Text>News & Events</Text>
+        </Button>
+        <Button block light full large style={styles.button} onPress={() => navigate('LocalChat', { name: 'Jane' })}>
+          <Text> Local Chat</Text>
+        </Button>
+      </View>
+    </MainLayout>
+  )
 }
 
+MenuScreen.navigationOptions = {
+  title: 'Welcome to Menu Screen',
+}
 
 const styles = StyleSheet.create({
-  text:{
-    textTransform:'uppercase',
-    textAlign:'center',
-  }, 
-  button:{
-     marginTop: 10,
+  button: {
+    marginTop: 10,
+    padding: 30,
   },
   container: {
-    flex:1,
+    flex: 1,
     justifyContent: 'space-around',
-    borderColor:'red',
-    borderStyle:'solid',
-    borderWidth:8,
-    //alignItems: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
+
 export default MenuScreen;
 
 
