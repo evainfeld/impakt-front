0b86b2ae52cf589f91b2dcb1bbd6536c05aab675
Basia Klosowska <basia.m.klosowska@gmail.com>
2019-11-07 21:38:11 +0100

    Add multiline text input with default editable value #1

--- src/screens/ConvinceFriends/Messanger.js
+++ src/screens/ConvinceFriends/Messanger.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text } from 'native-base';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
 
 import MainLayout from 'components/layouts/MainLayout.js'
 
 const Messanger = (props) => {
   const topic = props.navigation.state.params.topic;
+  const [value, onChangeText] = useState(topic.defaultMessages[0]);
 
   return (
     <MainLayout>
       <Text style={styles.text}>
+        Let's convince your friends who value:
+      </Text>
+      <Text style={styles.text}>
         {topic.title}
       </Text>
+      <TextInput
+        style={styles.textInput}
+        multiline
+        onChangeText={text => onChangeText(text)}
+        value={value}
+        maxLength={2000}
+      />
     </MainLayout>
   )
 }
@@ -26,8 +37,16 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontSize: 20,
     textAlign: 'center',
-    marginBottom: 20,
     textTransform: 'uppercase',
+  },
+  textInput: {
+    flex: 1,
+    marginTop: 40,
+    marginBottom: 40,
+    padding: 20,
+    fontSize: 16,
+    backgroundColor: '#ffffff',
+    textAlignVertical: 'top'
   }
 });
 
